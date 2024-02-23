36714f0ebe509af84167e0b72c2f76fe2485f723
ousseynoukone <ousseynou781227@gmail.com>
2024-02-23 19:27:44 +0000

    nickel

--- src/app/portfolio/static/layout/layout.component.ts
+++ src/app/portfolio/static/layout/layout.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./layout.component.css']
 })
 export class LayoutComponent {
+  showScrollToTopButton = false;
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    // Check the scroll position
+    const yOffset = window.pageYOffset || document.documentElement.scrollTop;
+    const scrollThreshold = 200; // Adjust this threshold as needed
+
+    // Show/hide the scroll-to-top button based on the scroll position
+    this.showScrollToTopButton = yOffset > scrollThreshold;
+  }
+
+  goToTop() {
+    // Smooth scroll to the top
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
