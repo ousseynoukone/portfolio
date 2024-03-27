d9ed1018eac6b7d7d4685f26b390c09f3bf5ca78
ousseynoukone <ousseynou781227@gmail.com>
2024-03-27 18:42:34 +0000

    animation fix

--- src/app/portfolio/pages/home/home.component.ts
+++ src/app/portfolio/pages/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  displayAnimationFlag : boolean = false;
+  ngOnInit(): void {
+    this.displayImage()
+  }
 
+  displayImage() {
+    const screenWidth = window.innerWidth;
+    if(screenWidth >= 400 ) {
+      this.displayAnimationFlag = true;
+    } else {
+      this.displayAnimationFlag = false;
+    }
+  }
 }
